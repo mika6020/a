09f4f979436d2e841aa3f7f62d85376431d750ea
skort11 <khalil.tayefe@gmail.com>
2016-12-13 20:52:06 +0330

    skort

--- plugins/lock_reply.lua
+++ plugins/lock_reply.lua
@@ -10,23 +10,23 @@ local function pre_process(msg)
     
 local function run(msg, matches)
     chat_id =  msg.to.id
-    if is_momod(msg) and matches[1] == 'lock' or matches[1] == 'قفل کردن' then
+    if is_momod(msg) and matches[1] == 'lock' or matches[1] == 'قفل' then
                     local hash = 'muter:'..msg.to.id
                     redis:set(hash, true)
-                    return "》ریپلی در گروه قفل شد."
+                    return ""
   elseif is_momod(msg) and matches[1] == 'unlock' or matches[1] == 'بازکردن'
 then
                     local hash = 'muter:'..msg.to.id
                     redis:del(hash)
-                    return "》ریپلی در گروه آزاد شد."
+                    return ""
 end
 end
 return {
     patterns = {
-        '^[/!#](lock) reply$',
-        '^[/!#](unlock) reply$',
-        '^[/!#](قفل کردن) ریپلی$',
-        '^[/!#](بازکردن) ریپلی$'
+        '^(lock) reply$',
+        '^(unlock) reply$',
+        '^(قفل) ریپلی$',
+        '^(بازکردن) ریپلی$'
     },
     run = run,
     pre_process = pre_process
