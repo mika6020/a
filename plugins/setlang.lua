e27aed94558c9c85c01a315b148857db284eaef6
skort11 <khalil.tayefe@gmail.com>
2016-12-13 22:05:55 +0330

    skort

--- plugins/setlang.lua
+++ plugins/setlang.lua
@@ -2,7 +2,7 @@ do
 local function run(msg, matches, callback, extra)
 local hash = 'group:'..msg.to.id
 local group_lang = redis:hget(hash,'lang')
-if matches[1] == 'setlang' and  matches[2] == 'en' and is_owner(msg) then 
+if matches[1] == 'setlang' and  matches[2] == 'en' or matches[1] =='تنظیم زبان' and matches[2] =='انگلیسی' and is_owner(msg) then 
     
    redis:hdel(hash,'lang')
         return 'group lang set to : en'
@@ -10,11 +10,11 @@ end
 
 
 
-if matches[1] == 'setlang' and matches[2] == 'fa' and is_owner(msg) then
+if matches[1] == 'setlang' and matches[2] == 'fa' or matches[1] =='تنظیم زبان' and matches[2] =='فارسی' and is_owner(msg) then
 redis:hset(hash,'lang',matches[2])
-        return 'زبان گروه تنظیم شد به : FA'
+        return 'زبان گروه تنظیم شد به : فارسی'
 end
-if matches[1] == 'lang' then
+if matches[1] == 'lang' or matches[1] =='زبان گروه' then
 if group_lang then 
 return "زبان گروه شما هم اکنون بر روی فارسی قرار دارد"
 else
@@ -24,12 +24,13 @@ end
 end
 return {
   patterns = {
-    "^[!#/]([Ss][Ee][Tt][Ll][Aa][Nn][Gg]) ([Ff][Aa])$",
-  "^[!#/]([Ss][Ee][Tt][Ll][Aa][Nn][Gg]) ([Ee][Nn])$",
-  "^[!#/]([Ll][Aa][Nn][Gg])"
+    "^([Ss][Ee][Tt][Ll][Aa][Nn][Gg]) ([Ff][Aa])$",
+  "^([Ss][Ee][Tt][Ll][Aa][Nn][Gg]) ([Ee][Nn])$",
+   "^(تنظیم زبان) (فارسی)$",
+  "^(تنظیم زبان) (انگلیسی)$",
+  "^(زبان گروه)",
+  "^([Ll][Aa][Nn][Gg])"
   },
   run = run
 }
 end
---edit by @CliApi
---Our channel : @create_antispam_bot
