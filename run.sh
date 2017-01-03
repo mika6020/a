f0e16819bf6c607d6ae946146a49efc71eb000b1
skort11 <khalil.tayefe@gmail.com>
2017-01-03 19:50:12 +0330

    SKORT

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@ killall screen
 killall tmux
 killall telegram-cli
 sudo killall
-screen ./launch.sh
 RED='\033[0;31m'
 NC='\033[0m'
 CYAN='\033[0;36m'
-echo -e "${CYAN}Install Completed! input ./run for run bot ${NC}"
+echo -e "${CYAN}Install Completed! input screen ./launch.sh for run bot ${NC}"
 exit
 
