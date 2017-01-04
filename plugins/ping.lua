cb8f847d98eaad6fa31676b2c6ff4c64d67a8315
skort11 <khalil.tayefe@gmail.com>
2017-01-04 18:47:35 +0330

    Update ping.lua

--- plugins/ping.lua
+++ plugins/ping.lua
@@ -1,27 +1,27 @@
 local timsar = {
-'❤️ آنلاینم عمو شهرام 😐',
-'❤️ عمو شهرام آنلاینم 😐',
+'❤️ آنلاینم  😐',
+'❤️ آنلاینم 😐',
 },
 'local' function run(msg, matches)
 if msg.from.id == 279357744 then 
 local text = timsar[math.random(#timsar)]
 return reply_msg(msg.id, text, ok_cb, false)
 elseif msg.from.id == 229411414 then
-  return "❤️ آنلاینم بابا شهرام 😐"
+  return "❤️ آنلاینم 😐"
 elseif msg.from.id == 198986344 then
-  return "❤️ جون جون هستم  خاله شمیم 😍"
+  return "❤️ آنلاینم 😍"
   elseif msg.from.id == 267885857 then
-  return "❤️ آنلاینم خاله نرگس  ✌ 🌺"
+  return "❤️ آنلاینم  ✌ 🌺"
   elseif msg.from.id == 82082411 then
-  return "❤️ هستم عمو رضا 😅"
+  return "❤️ آنلاینم 😅"
 elseif msg.from.id == 70018683 then
-  return "❤️ آنلاینم خاله پروانه  ✌ 🌺"
+  return "❤️ آنلاینم  ✌ 🌺"
 elseif is_admin1(msg) then
- return "❤️ آنلاینم عمو 😐"
+ return "❤️ آنلاینم 😐"
 elseif is_owner(msg) then
-  return " 😐 ✌ هستم در خدمتتون ✌ 😐"
+  return " 😐 آنلاینم 😐"
 elseif is_momod(msg) then
- return "😐 هستمـ ولے خستمـ 🎈"
+ return "😐 آنلاینم 🎈"
 else 
 return 
 end
